8344f9866fd14bc55ed09f09d0d784bcddc8e58b
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-01-31 19:53:11 +0300

    feat: add Projects component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
-
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
     <Home/>
     <About />
     <Services />
+    <Projects />
     </>
     
   );
