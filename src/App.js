771784687de88b8b9079f5f5f054abf62a9a9002
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-01-31 19:54:01 +0300

    feat: add Contact component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
     <About />
     <Services />
     <Projects />
+    <Contact />
     </>
     
   );
